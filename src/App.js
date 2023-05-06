3778f68ee3ea022e81063b25bec39e83e6cfc6a1
Destaritgithub <alexdesse@gmail.com>
2023-05-05 23:41:02 -0700

    connection with firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './component/Navbar';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
   return (
     <>
-      <h1> my App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forget-Password' element={<ForgotPassword />} />
+          {/* <Route path='/*' element={<ForgotPassword />} /> */}
+        </Routes>
+        <Navbar />
+      </Router>
+      <ToastContainer />
     </>
   );
 }
